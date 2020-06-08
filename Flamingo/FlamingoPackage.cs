b8d41642673d592f24ecdba8b95c3807fdfd47ae
esrmnt <nitish.er@gmail.com>
2020-06-09 00:49:57 +0530

    Added menu items to extension

--- Flamingo/FlamingoPackage.cs
+++ Flamingo/FlamingoPackage.cs
@@ -25,6 +25,7 @@ namespace Flamingo
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(FlamingoPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class FlamingoPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,8 @@ namespace Flamingo
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await XSLTRunCommand.InitializeAsync(this);
+            await XSLTDebugCommand.InitializeAsync(this);
         }
 
         #endregion
