757bf9d4fefe702e25ea494cad2ce883e44fb3c1
esrmnt <nitish.er@gmail.com>
2020-06-21 18:13:20 +0530

    Added xslt transformation using saxon-api-he

--- Flamingo/XSLTRunCommand.cs
+++ Flamingo/XSLTRunCommand.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Reflection;
 using System.ComponentModel.Design;
+using System.IO;
+using System.Windows.Media;
+using EnvDTE;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
+using Saxon.Api;
 using Task = System.Threading.Tasks.Task;
 
 namespace Flamingo
@@ -47,19 +52,49 @@ namespace Flamingo
         private void Execute(object sender, EventArgs e)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
-            string message = string.Format($"{Settings.Default.Version} : {Settings.Default.Input} : {Settings.Default.Output}");
-            string title = "XSLTRunCommand";
+            string styleSheetFileName = GetActiveDocument();
+            string extension = Path.GetExtension(styleSheetFileName);
 
-            CommonOperations.logInformation();
+            if (Path.GetExtension(styleSheetFileName).Equals(".xslt") || Path.GetExtension(styleSheetFileName).Equals(".xsl"))
+            {
+                Transform(styleSheetFileName, Settings.Default.Input, Settings.Default.Output);
+            }
+            else 
+            {
+                string infoMessage = $"Select the stylesheet as the active document, before running Flamingo";
+                VsShellUtilities.ShowMessageBox(
+                    this.package,
+                    infoMessage,
+                    "Flamiongo",
+                    OLEMSGICON.OLEMSGICON_INFO,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+        }
 
-            // Show a message box to prove we were here
-            VsShellUtilities.ShowMessageBox(
-                this.package,
-                message,
-                title,
-                OLEMSGICON.OLEMSGICON_INFO,
-                OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        public static void Transform(string styleSheet, string inputFile, string outputFile) 
+        {
+            Processor processor = new Processor();
+            var uriInput = new System.Uri(inputFile);            
+            var uriStyleSheet = new System.Uri(styleSheet);
+            
+            XdmNode inputXdmNode = processor.NewDocumentBuilder().Build(uriInput);
+            XsltCompiler xsltCompiler = processor.NewXsltCompiler();
+            XsltExecutable xsltExecutable = xsltCompiler.Compile(uriStyleSheet);
+            XsltTransformer xsltTransformer = xsltExecutable.Load();
+            xsltTransformer.InitialContextNode = inputXdmNode;
+
+            Serializer serializer = processor.NewSerializer();
+            StreamWriter streamWriter = new StreamWriter(outputFile);
+            serializer.SetOutputWriter(streamWriter);
+            xsltTransformer.Run(serializer);
+            streamWriter.Close();
+        }       
+
+        public static string GetActiveDocument()
+        {
+            EnvDTE80.DTE2 dteObject = Package.GetGlobalService(typeof(Microsoft.VisualStudio.Shell.Interop.SDTE)) as EnvDTE80.DTE2;
+            return dteObject.ActiveDocument.FullName;            
         }
     }
 }
